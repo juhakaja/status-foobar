4a25de70c8f391527a682691090ed6eba4ac0b5a
Juha Kajava <juhakaja@gmail.com>
2013-03-22 23:53:52 +0900

    Add time to datetime.

--- status-foobar/datetime.c
+++ status-foobar/datetime.c
@@ -3,5 +3,5 @@ void datetime(char *current_time)
   time_t now = time(NULL);
   struct tm * dts = localtime(&now);
 
-  strftime(current_time, 1000, "%A, %B %d %Y", dts);
+  strftime(current_time, 1000, "%a, %b %d %H:%M:%S %Z %Y", dts);
 }
