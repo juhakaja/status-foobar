044c9cd354fc58f75ac05e4c19ce01736d009bb4
Juha Kajava <juhakaja@gmail.com>
2013-03-23 00:12:44 +0900

    For loop sleep 1.

--- status-foobar/main.c
+++ status-foobar/main.c
@@ -10,11 +10,13 @@ main()
   char hostname[10];
   char battery[4];
 
-  host(hostname);
-  datetime(current_time);
-  battery_status(battery);
+  for (;;sleep(1)) {
+    host(hostname);
+    datetime(current_time);
+    battery_status(battery);
 
-  printf("host: %s | battery: %s | %s\n", hostname, battery, current_time);
+    printf("host: %s | battery: %s | %s\n", hostname, battery, current_time);
+  }
 
   return 0;
 }
