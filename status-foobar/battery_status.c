93ab3e2c523550c3fd39680ad203cbfa23852670
Juha Kajava <juhakaja@gmail.com>
2013-03-23 01:15:50 +0900

    Clean tabs.

--- status-foobar/battery_status.c
+++ status-foobar/battery_status.c
@@ -1,33 +1,33 @@
 float energy_full()
 {
-	FILE *fd;
-	int energy_full;
-
-	fd = fopen("/sys/class/power_supply/BAT0/energy_full", "r");
-	if(fd == NULL) {
-		fprintf(stderr, "Error opening energy_full.\n");
-		return -1;
-	}
-	fscanf(fd, "%d", &energy_full);
-	fclose(fd);
-
-	return energy_full;
+  FILE *fd;
+  int energy_full;
+  
+  fd = fopen("/sys/class/power_supply/BAT0/energy_full", "r");
+  if(fd == NULL) {
+  	fprintf(stderr, "Error opening energy_full.\n");
+  	return -1;
+  }
+  fscanf(fd, "%d", &energy_full);
+  fclose(fd);
+  
+  return energy_full;
 }
 
 float energy_now()
 {
-	FILE *fd;
-	int energy_now;
-
-	fd = fopen("/sys/class/power_supply/BAT0/energy_now", "r");
-	if(fd == NULL) {
-		fprintf(stderr, "Error opening energy_now.\n");
-		return -1;
-	}
-	fscanf(fd, "%d", &energy_now);
-	fclose(fd);
-
-	return energy_now;
+  FILE *fd;
+  int energy_now;
+  
+  fd = fopen("/sys/class/power_supply/BAT0/energy_now", "r");
+  if(fd == NULL) {
+  	fprintf(stderr, "Error opening energy_now.\n");
+  	return -1;
+  }
+  fscanf(fd, "%d", &energy_now);
+  fclose(fd);
+  
+  return energy_now;
 }
 
 void battery_status(char *battery) {
